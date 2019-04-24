de490523cb85b658d909309113ada06ec3016e6f
pavelp21 <pavelvp2182@yandex.ru>
2019-04-24 15:55:53 +0500

    Mail section is done

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -1,7 +1,58 @@
 //@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
 
 $(document).ready(function() {
-
+    $(function () {
+        var check = $('.check', this),
+            email = $('.email', this),
+            message = $('.alert_message', this),
+            button = $('.button_mail', this);
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert_message', this),
+                reNone = /.+/,
+                email = $('.email', this),
+                button = $('.button_mail', this);
+            if (!email.val().match(reNone)) {
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
+                // button.text('Отправляем...');
+                button.text('Отправляем...');
+                setTimeout(function () {
+                    button.text('Отправлено!');
+                }, 500);
+            }
+        });
+        email.click(function () {
+            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+            message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#eee",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#eee",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
+    });
 
 
 /*Конец документа*/
