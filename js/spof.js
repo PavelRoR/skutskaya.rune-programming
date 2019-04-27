62e90f4e3e38e1dd5224ef3723b518b9349e3b16
pavelp21 <pavelvp2182@yandex.ru>
2019-04-27 14:22:04 +0500

    Special offer is done and adepted

---
+++ js/spof.js
@@ -0,0 +1,10 @@
+
+    var isIE = false || !!document.documentMode;
+
+    if (isIE) {
+        var head  = document.getElementsByTagName("head")[0];
+        var link  = document.createElement("link");
+        link.rel  = "stylesheet";
+        link.href = "../css/spof_ie.css";
+        head.appendChild(link);
+    }
